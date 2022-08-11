cbcd498438aaf76c736528301fcb620faab947c5
mbuckley5689 <41401573+mbuckley5689@users.noreply.github.com>
2022-08-11 16:27:39 -0400

    Update mario.c

--- 2022/Week 1/mario.c
+++ 2022/Week 1/mario.c
@@ -1,1 +1,49 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int state = 1;
+    while (state == 1)
+    {
+        // request a height for the mario blocks
+        int height = get_int("Input an integer between 1 and 8 (including thoes numbers): \n");
+
+        // initialize a variable to properly space the mario blocks
+        int spaces = height - 1;
+
+        // run the mario block loop
+        if (height > 0 && height < 9)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = spaces; i < j; j--)
+                {
+                    printf(" ");
+                }
+
+                for (int j = 0; j <= i; j++)
+                {
+                    printf("#");
+                }
+
+                printf("  ");
+
+                for (int j = 0; j <= i; j++)
+                {
+                    printf("#");
+                }
+
+                printf("\n");
+            }
+            state = 0;
+        }
+
+        // explain to the user that they incorrectly answered the prompt.
+        // the while-loop will now repeat and ask the user for a height again.
+        else
+        {
+            printf("Your input was invalid. Please follow the prompt.\n");
+        }
+    }
+}
