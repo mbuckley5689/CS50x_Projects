b70907dfe51ba32580b0748d5ebf2d56b40a3802
mbuckley5689 <41401573+mbuckley5689@users.noreply.github.com>
2022-08-11 16:31:46 -0400

    Update hello.c
    Added a description of the purpose of the code.

--- 2022/Week 1/hello.c
+++ 2022/Week 1/hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// The purpose of this code is to complete the "hello" assignment from the week 1 problem set for
+// the 2022 session of the CS50: Introduction to Computer Science course. The prompt for the assignment can be accessed via the URL below:
+
+// https://cs50.harvard.edu/x/2022/psets/1/hello/
+
 // prompt the user for their name, and then welcome them using their name.
 int main(void)
 {
