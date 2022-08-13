ef002b5e86a32138bf6dc6d4b70f76b6ad67950d
mbuckley5689 <41401573+mbuckley5689@users.noreply.github.com>
2022-08-13 12:20:11 -0400

    Update credit.c
    The current revision of this code passes the requirements set by the prompt. However, the card check functions (verify_amex, etc.) could be simplified. The function "countdigit" could be used to check for the card length instead of using the ranged "if" statement to check to see if the card has the correct amount of digits.

--- 2022/Week 1/credit.c
+++ 2022/Week 1/credit.c
@@ -11,6 +11,9 @@
 int digitcount(long number);
 long indexdigit(long number, int index);
 int luhns(long sixteen_digit_number);
+int check_mastercard(long sixteen_digit_number);
+int check_amex(long sixteen_digit_number);
+int check_visa(long sixteen_digit_number);
 
 int main(void)
 {
@@ -18,22 +21,66 @@ int main(void)
     // define the base of the number system being used.
     int base = 10;
     // define the amount of digits required for a credit card number
-    int digitreq = 16;
+    int digitreq_amex = 15;
+    int digitreq_mastercard = 16;
+    int digitreq_visa_one = 13;
+    int digitreq_visa_two = 16;
     // define minimum and maximum credit card numbers
-    long ccmin = pow(base, digitreq);
-    long ccmax = pow(base, digitreq + 1);
-    ccmax = ccmax - 1;
+    long amex_min = pow(base, digitreq_amex);
+    long amex_max = pow(base, digitreq_amex + 1) - 1;
+    long mastercard_min = pow(base, digitreq_mastercard);
+    long mastercard_max = pow(base, digitreq_mastercard + 1) - 1;
+    long visa_one_min = pow(base, digitreq_visa_one);
+    long visa_one_max = pow(base, digitreq_visa_one + 1) - 1;
+    long visa_two_min = pow(base, digitreq_visa_two);
+    long visa_two_max = pow(base, digitreq_visa_two + 1) - 1;
+
     // ask the user for their credit card number.
     // repeat the inquiry if they input an invalid number.
-    //long ccnumber = get_long("Please enter your credit card number: \n");
-    long ccnumber = 4003600000000014;
-    while (ccnumber < 1000000000000000 || ccnumber > 9999999999999999)
+    long ccnumber = get_long("Please enter your credit card number: \n");
+
+    // verify whether the credit card number is valid or not via Luhn's Algorithm
+    // luhns integer indicates the number that the algorithm must output for the
+    // credit card number to be valid
+    int luhns_modulo = 10;
+    int verify_luhns = luhns(ccnumber);
+    //printf("luhns number: %i\n", verify_luhns);
+
+    if (verify_luhns % luhns_modulo == 0)
     {
-        ccnumber = get_long("Please enter your credit card number: \n");
+        // check to see if the credit card may be an American Express
+        int verify_amex = check_amex(ccnumber);
+        if (verify_amex == 1)
+        {
+            printf("AMEX\n");
+        }
+        // check to see if the credit card may be a Mastercard
+        int verify_mastercard = check_mastercard(ccnumber);
+        if (verify_mastercard == 1)
+        {
+            printf("MASTERCARD\n");
+        }
+
+        // check to see if the credit card may be a VISA
+        int verify_visa = check_visa(ccnumber);
+        if (verify_visa == 1)
+        {
+            printf("VISA\n");
+        }
+
+        // check to see if the credit card is valid or not
+        if (verify_amex != 1 && verify_mastercard != 1 && verify_visa != 1)
+        {
+            printf("INVALID\n");
+        }
     }
 
-    int verify_twenty = luhns(ccnumber);
-    printf("Verification Number: %i\n", verify_twenty);
+    if (verify_luhns % luhns_modulo != 0)
+    {
+        printf("INVALID\n");
+    }
+
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,7 +94,7 @@ int digitcount(long number)
     int digit = 0;
     long base = 10;
     long base_now = base;
-    while (check > 0)
+    while (check >= 0)
     {
         digit = digit + 1;
         check = number;
@@ -90,8 +137,10 @@ int luhns(long sixteen_digit_number)
     // the loop starts at the left-most digit and ends with the right-most
     for (int i = ccnumber_max_index; i > -1; i--)
     {
+        //printf("current index: %i\n", i);
         // initializes the current credit card number in the sweep
         int current_index = indexdigit(sixteen_digit_number, i);
+        //printf("index value: %i\n", current_index);
 
         // enters the condition for doing operations on every-other number
         // in the credit card number, if you started counting from the right-side of the number.
@@ -99,14 +148,18 @@ int luhns(long sixteen_digit_number)
 
         // this loop will multiply every other number by two. each digit of each of those products
         // are to then added to the "checker" total.
-        if (i % 2 != 0)
+        if ((ccnumber_max_index % 2 != 0 && i % 2 != 0) || (ccnumber_max_index % 2 == 0 && i % 2 != 0))
         {
             int intermediate_addition = current_index * 2;
+            //printf("intermediate addition: %i\n", intermediate_addition);
             int intermediate_max_index = digitcount(intermediate_addition) - 1;
+            //printf("intermediate max index: %i\n", intermediate_max_index);
             for (int j = intermediate_max_index; j > -1; j--)
             {
-            int intermediate_index = indexdigit(intermediate_addition, j);
-            total = total + intermediate_index;
+                int intermediate_index = indexdigit(intermediate_addition, j);
+                //printf("intermediate index: %i\n", intermediate_index);
+                total = total + intermediate_index;
+                //printf("total: %i\n", total);
             }
         }
         // for all indices excluded in the previous "every-other" indexing and summing,
@@ -114,7 +167,104 @@ int luhns(long sixteen_digit_number)
         else
         {
             total = total + current_index;
+            //printf("total: %i\n", total);
         }
     }
     return total;
+}
+
+// check to see if a sixteen digit credit card number is a mastercard
+// (assuming the number passes Luhn's Algorithm)
+int check_mastercard(long sixteen_digit_number)
+{
+    int index_one = 15;
+    int index_two = 14;
+    int verify_mastercard = 0;
+    // define the base of the number system being used.
+    int base = 10;
+    // define the amount of digits required for a credit card number
+    int digitreq_mastercard = 16;
+    // define minimum and maximum credit card numbers
+    long mastercard_min = pow(base, digitreq_mastercard - 1);
+    long mastercard_max = pow(base, digitreq_mastercard);
+    //printf("mastercard min: %lu\n", mastercard_min);
+    //printf("mastercard max: %lu\n", mastercard_max);
+
+    int digit_one = indexdigit(sixteen_digit_number, index_one);
+    int digit_two = indexdigit(sixteen_digit_number, index_two);
+    if (digit_one == 5 && (digit_two == 1 || digit_two == 2 || digit_two == 3 || digit_two == 4 || digit_two == 5))
+    {
+        verify_mastercard = 1;
+    }
+
+    if (sixteen_digit_number < mastercard_min || sixteen_digit_number >= mastercard_max)
+    {
+        verify_mastercard = 0;
+    }
+
+    return verify_mastercard;
+}
+
+// check to see if a sixteen digit credit card number is an american express
+// (assuming the number passes Luhn's Algorithm)
+int check_amex(long sixteen_digit_number)
+{
+    int index_one = 14;
+    int index_two = 13;
+    int verify_amex = 0;
+    // define the base of the number system being used.
+    int base = 10;
+    // define the amount of digits required for a credit card number
+    int digitreq_amex = 15;
+    // define minimum and maximum credit card numbers
+    long amex_min = pow(base, digitreq_amex - 1);
+    long amex_max = pow(base, digitreq_amex);
+
+    int digit_one = indexdigit(sixteen_digit_number, index_one);
+    //printf("Digit 1: %i\n", digit_one);
+    int digit_two = indexdigit(sixteen_digit_number, index_two);
+    //printf("Digit 2: %i\n", digit_two);
+    if (digit_one == 3 && (digit_two == 4 || digit_two == 7))
+    {
+        verify_amex = 1;
+    }
+
+    if (sixteen_digit_number < amex_min || sixteen_digit_number >= amex_max)
+    {
+        verify_amex = 0;
+    }
+    return verify_amex;
+}
+
+// check to see if a sixteen digit credit card number is an american express card
+// (assuming the number passes Luhn's Algorithm)
+int check_visa(long sixteen_digit_number)
+{
+    int verify_visa = 0;
+    int index_one = digitcount(sixteen_digit_number) - 1;
+    int index_two = index_one - 1;
+
+    // define the base of the number system being used.
+    int base = 10;
+    // define the amount of digits required for a credit card number
+    int digitreq_visa_one = 13;
+    int digitreq_visa_two = 16;
+    // define minimum and maximum credit card numbers
+    long visa_one_min = pow(base, digitreq_visa_one - 1);
+    long visa_one_max = pow(base, digitreq_visa_one);
+    long visa_two_min = pow(base, digitreq_visa_two - 1);
+    long visa_two_max = pow(base, digitreq_visa_two);
+
+    int digit_one = indexdigit(sixteen_digit_number, index_one);
+    if (digit_one == 4)
+    {
+        verify_visa = 1;
+    }
+    if ((sixteen_digit_number < visa_one_min || sixteen_digit_number >= visa_one_max) &&
+        (sixteen_digit_number < visa_two_min || sixteen_digit_number >= visa_two_max))
+    {
+        verify_visa = 0;
+    }
+
+    return verify_visa;
 }
